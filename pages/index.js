bf523720eaa3b3e23ad03e64d88f30ee094a8c37
siam <samisiam851@gmail.com>
2023-11-25 20:04:13 +0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,22 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+
+      <Head>
+        {/* Add title to the page */}
+        <title>My page</title>
+
+        {/* Add meta tags for description, keywords, etc. */}
+        <meta name="description" content="Your page description" />
+        <meta name="keywords" content="keyword1, keyword2, keyword3" />
+
+        {/* Include external stylesheet */}
+        <link rel="stylesheet" href="/path/to/your/stylesheet.css" />
+
+        {/* Add other meta tags or links as needed */}
+      </Head>
+
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
