e9c52770155714458004d7a3bd98ce1a32f7a309
siam <samisiam851@gmail.com>
2023-11-29 18:08:47 +0600

    SSR

--- pages/blogpost/[slug].jsx
+++ pages/blogpost/[slug].jsx
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
+// import { getServerSideProps } from 'next';
 
-const BlogPost = () => {
+const BlogPost = (props) => {
     const router = useRouter();
     const { slug } = router.query
-    const [blog, setBlog] = useState({});
-    console.log(slug);
-    useEffect(() => {
+    const [blog, setBlog] = useState(props.myBlog);
+    // console.log(slug);
+    // useEffect(() => {
 
-        if (router.isReady) {
-            axios.get(`http://localhost:3000/api/blogs/blog?slug=${slug}`).then(res => setBlog(res.data))
-        }
+    //     if (router.isReady) {
+    //         axios.get(`http://localhost:3000/api/blogs/blog?slug=${slug}`).then(res => setBlog(res.data))
+    //     }
         
-    }, [router.isReady]);
+    // }, [router.isReady]);
     return (
         <div className='text-center'>
             <h3 className='text-4xl'>{blog.title}</h3>
@@ -23,4 +24,13 @@ const BlogPost = () => {
     );
 }
 
+export async function getServerSideProps  (context){
+    const {slug} = context.query
+
+console.log(slug); 
+   const res = await axios.get(`http://localhost:3000/api/blogs/blog?slug=${slug}`)
+   console.log('heeeeeeeeeeeeeeeeeeeeeeey',res.data);
+   const myBlog = res.data
+   return {props : {myBlog}}
+}
 export default BlogPost;
--- pages/blogs.jsx
+++ pages/blogs.jsx
@@ -1,22 +1,24 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
-const blogs = () => {
-    const [blogs, setBlogs] = useState([]);
-    useEffect(() => {
-       axios.get('/hello').then(res => setBlogs(res.data?.blogs)).catch(err => console.log(err))
-    }, []);
+const blogs = (props) => {
+    const [blogs, setBlogs] = useState(props.myBlogs);
+   
 
     console.log(blogs);
     return (
         <div >
             {blogs?.map(blog =>
                 <div className='border rounded-lg py-10 px-3 my-3 w-fit mx-auto'>
-                   <Link href={`/blogpost/${blog.slug}`}> <h3 className='text-xl text-center font-semibold'>{blog?.title}</h3></Link>
+                    <Link href={`/blogpost/${blog.slug}`}> <h3 className='text-xl text-center font-semibold'>{blog?.title}</h3></Link>
                     <p className='text-gray-500'>{blog?.body.substr(0, 60)}...</p>
                 </div>)}
         </div>
     );
 };
-
+export const getServerSideProps = async (context) => {
+    const res = await axios.get('/hello')
+    const myBlogs = res.data.blogs
+    return { props: {myBlogs} }
+}
 export default blogs;
